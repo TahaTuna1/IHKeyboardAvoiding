7c7801db61d3d0adc179bc8167e76fe743949e04
Taha Tuna <119931873+TahaTuna1@users.noreply.github.com>
2026-04-13 08:20:24 +0300

    Add Swift Package Manager support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version: 5.6
 import PackageDescription
 
 let package = Package(
-    name: "IHKeyboardAvoiding"
+    name: "IHKeyboardAvoiding",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "IHKeyboardAvoiding",
+            targets: ["IHKeyboardAvoiding"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "IHKeyboardAvoiding",
+            dependencies: [],
+            path: "Sources"
+        )
+    ]
 )
